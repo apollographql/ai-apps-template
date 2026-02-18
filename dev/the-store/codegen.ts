96748b6da84b38e657e21e371741dd9f36061bc5
Jerel Miller <jerelmiller@gmail.com>
2026-02-18 16:09:06 -0700

    Dedupe operation suffix

--- dev/the-store/codegen.ts
+++ dev/the-store/codegen.ts
@@ -30,6 +30,9 @@ const config: CodegenConfig = {
         // Apollo Client doesn't add the `__typename` field to root types so
         // don't generate a type for the `__typename` for root operation types.
         skipTypeNameForRoot: true,
+        // Avoid adding an operation suffix to avoid double `QueryQuery` names
+        // for operations that already contain the operation suffix
+        dedupeOperationSuffix: true,
       },
     },
   },
