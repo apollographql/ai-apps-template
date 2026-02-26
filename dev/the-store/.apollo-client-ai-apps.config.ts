66fa0eac2768142adb31155cb356daf8feb25e52
Jerel Miller <jerelmiller@gmail.com>
2026-02-25 18:57:05 -0700

    Remove entry in config

--- dev/the-store/.apollo-client-ai-apps.config.ts
+++ dev/the-store/.apollo-client-ai-apps.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "@apollo/client-ai-apps/config";
 
 export default defineConfig({
-  entry: {
-    staging: "https://my-awesome-site.com",
-  },
   csp: {
     resourceDomains: ["https://cdn.dummyjson.com"],
   },
