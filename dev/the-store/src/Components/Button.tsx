313cb9bf98e55828d2ecdbbb9106d8b85a2d6c7b
Jerel Miller <jerelmiller@gmail.com>
2026-02-26 16:54:28 -0700

    More button styles

--- dev/the-store/src/Components/Button.tsx
+++ dev/the-store/src/Components/Button.tsx
@@ -2,8 +2,8 @@ import type { ComponentPropsWithoutRef } from "react";
 import { clsx } from "clsx";
 
 interface Props extends ComponentPropsWithoutRef<"button"> {
-  variant: "primary" | "secondary";
-  size?: "lg" | "md";
+  variant: "primary" | "secondary" | "hidden";
+  size?: "lg" | "md" | "sm";
 }
 
 export function Button({
@@ -17,14 +17,17 @@ export function Button({
     <button
       {...props}
       className={clsx(
-        className,
         "inline-flex items-center gap-2 font-semibold justify-center rounded-lg transition-colors",
+        "disabled:text-disabled disabled:bg-disabled disabled:cursor-not-allowed disabled:border-transparent",
         variant === "primary" &&
           "bg-button-primary hover:bg-button-primary-hover",
         variant === "secondary" &&
           "border border-primary bg-button-secondary hover:bg-button-secondary-hover",
-        size === "lg" && "px-4 py-3 h-12 font-base",
-        size === "md" && "px-3 py-2 h-10"
+        variant === "hidden" && "hover:bg-button-secondary-hover",
+        size === "lg" && "px-4 py-3 h-12 text-base",
+        size === "md" && "px-3 py-2 h-10 text-sm",
+        size === "sm" && "px-3 py-2 h-8 text-sm",
+        className
       )}
     >
       {children}
