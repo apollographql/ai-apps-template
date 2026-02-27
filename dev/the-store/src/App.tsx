40aade985b330b7b5e74ef9254a035e5c47e355b
Jerel Miller <jerelmiller@gmail.com>
2026-02-26 19:27:36 -0700

    Use platform select

--- dev/the-store/src/App.tsx
+++ dev/the-store/src/App.tsx
@@ -5,6 +5,7 @@ import Cart from "./Components/Cart";
 import SearchBar from "./Components/SearchBar";
 import SearchResults from "./Components/SearchResults";
 import { Link, MemoryRouter, Route, Routes } from "react-router";
+import { Platform } from "@apollo/client-ai-apps";
 import { useToolInput, useToolName } from "@apollo/client-ai-apps/react";
 import { useState } from "react";
 import { Logo } from "./Components/Logo";
@@ -42,7 +43,12 @@ function App() {
         <div className="flex items-center justify-between gap-2 mb-4">
           <Link to="/home" className="flex gap-2 items-center">
             <Logo className="size-10 text-primary dark:text-white" />
-            <h1 className="text-2xl font-bold">Apollo Store</h1>
+            <h1 className="text-2xl font-bold">
+              {Platform.select({
+                mcp: "Apollo MCP Store",
+                openai: "Apollo ChatGPT Store",
+              })}
+            </h1>
           </Link>
           <Link to="/cart" className="font-medium flex items-center gap-1">
             <ShoppingCart size={20} />
