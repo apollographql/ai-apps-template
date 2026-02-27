22719b740456643688b33edb65902c0c0bc9ccfe
Jerel Miller <jerelmiller@gmail.com>
2026-02-26 17:53:40 -0700

    Fix text color

--- dev/the-store/src/Components/Button.tsx
+++ dev/the-store/src/Components/Button.tsx
@@ -24,10 +24,10 @@ export function Button({
         "inline-flex items-center gap-2 font-semibold justify-center rounded-lg transition-colors",
         "disabled:text-disabled disabled:bg-disabled disabled:cursor-not-allowed disabled:border-transparent",
         variant === "primary" &&
-          "bg-button-primary hover:bg-button-primary-hover",
+          "text-white bg-button-primary hover:bg-button-primary-hover",
         variant === "secondary" &&
-          "border border-primary bg-button-secondary hover:bg-button-secondary-hover",
-        variant === "hidden" && "hover:bg-button-secondary-hover",
+          "text-primary border border-primary bg-button-secondary hover:bg-button-secondary-hover",
+        variant === "hidden" && "text-primary hover:bg-button-secondary-hover",
         size === "lg" && "px-4 py-3 h-12 text-base",
         size === "md" && "px-3 py-2 h-10 text-sm",
         size === "sm" && "px-3 py-2 h-8 text-sm",
